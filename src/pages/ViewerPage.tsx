29e50f6cc0699c93aab0d7399185c1ff97b82aff
Manuel <34459259+mnlt@users.noreply.github.com>
2025-12-22 22:52:03 +0100

    new design7

--- src/pages/ViewerPage.tsx
+++ src/pages/ViewerPage.tsx
@@ -660,7 +660,7 @@ const styles: Record<string, React.CSSProperties> = {
     left: 0,
     right: 0,
     bottom: 0,
-    cursor: 'pointer',
+    cursor: 'default',
     zIndex: 10,
   },
   feedbackOverlayFullscreen: {
@@ -669,7 +669,7 @@ const styles: Record<string, React.CSSProperties> = {
     left: 0,
     right: 0,
     bottom: 0,
-    cursor: 'pointer',
+    cursor: 'default',
     zIndex: 10,
   },
 }
